c43b777310e4117b08870ff3f30a2a34647c9531
okaynik <40804093+okaynik@users.noreply.github.com>
2020-05-03 16:19:04 -0700

    Add files via upload

---
+++ EmployeeLoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmployeeLoginViewController.swift
+//  GroceryApp
+//
+//  Created by nikita on 5/2/20.
+//  Copyright © 2020 nikita. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
